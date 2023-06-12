45713585b5165d718f7a2a3619eac14bc3b97e5d
Alex Kachalkov <alexander.kachalkov@aec.gov.au>
2023-06-12 22:36:00 +1000

    migrated to netstandard 2.1

--- WrappedSqlFileStream/WrappedSqlFileStream.cs
+++ WrappedSqlFileStream/WrappedSqlFileStream.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
-using System.Data.SqlTypes;
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data.SqlTypes;
 using WrappedSqlFileStream.Mapping;
 
 namespace WrappedSqlFileStream
